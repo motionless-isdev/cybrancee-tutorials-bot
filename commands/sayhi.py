3889f7a09a2857fdbde49cb23bfde5f42c4c8d8b
motionless <ahrongm.gamasan@gmail.com>
2025-10-16 16:58:32 +0800

    i added a sayhi command

---
+++ commands/sayhi.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+class Hi(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.hybrid_command(name="sayhi", description="Another Hi")
+    async def sayhi(self, ctx):
+        """HIIIIIII"""
+        await ctx.send("Hiiiiii!")
+
+async def setup(bot):
+    await bot.add_cog(Hi(bot))
